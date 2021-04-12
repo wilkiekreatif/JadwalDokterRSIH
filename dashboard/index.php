db5d8434d933c8e2cfd2491ec8a662623b8e7107
wilkiekreatif <82008932+wilkiekreatif@users.noreply.github.com>
2021-04-12 11:34:58 +0700

    menggabungkan frontend ke repository

---
+++ dashboard/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jadwal Poliklinik</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="function.js"></script>
+</head>
+<body>
+
+
+	
+	<header>
+		<div class="top">
+			<h4 class="tanggal">Tanggal uptodate</h4>
+			<h4 class="jam">Jam sekarang</h4>
+			<h1>POLIKLINIK RUMAH SAKIT INTAN HUSADA</h1>
+		</div>
+	</header>
+
+
+	<div class="list-dokter">
+		<h2>JADWAL DOKTER HARI INI</h2>
+
+		<div class="dok">
+			<a target="_blank" href="img\Hendy Yogya.jpg">
+				<img src="img\Hendy Yogya.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Hendy Yogya, Sp.KJ(K)</div>
+			<h4>Spesialis Kejiwaan</h4>
+			<h3>Pukul : 16.00 - 19.00</h3>
+			<h5>- Status Pasien -</h5>
+			
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Arif Satria Prabowo 1x1.jpg">
+				<img src="img\Arif Satria Prabowo 1x1.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Arif Satria Prabowo, Sp.OG</div>
+			<h4>Spesialis Obgyn</h4>
+			<h3>Pukul : 12.00 - 14.00</h3>
+			<!-- <h3>Pukul : 11.00 - 13.00</h3> -->
+			<h5>- Status Pasien -</h5>
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Riki Vita THT.jpg">
+				<img src="img\Riki Vita THT.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Riki Vita W, SpTHT-KL</div>
+			<h4>Spesialis THT</h4>
+			<h3>Pukul : 14.00 - 16.00</h3>
+			<h5>- Status Pasien -</h5>
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Gustomo Panantro.jpg">
+				<img src="img\Gustomo Panantro.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Gustomo Panantro,Sp A, M.Kes</div>
+			<h4>Spesialis Anak</h4>
+			<h3>Pukul : 15.00 - 17.00</h3>
+			<!-- <h3>Pukul : 10.00 - 12.00</h3>
+			<h3>Pukul : 10.00 - 12.00</h3> -->
+			<h5>- Status Pasien -</h5>
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Hadiyana.jpg">
+				<img src="img\Hadiyana.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Hadiyana Suryadi, Sp.B</div>
+			<h4>Spesialis Dalam</h4>
+			<h3>Pukul : 07.00 - 09.00</h3>
+			<!-- <h3>Pukul : 10.00 - 12.00</h3>
+			<h3>Pukul : 10.00 - 12.00</h3> -->
+			<h5>- Status Pasien -</h5>
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Yanto Widiantoro.jpg">
+				<img src="img\Yanto Widiantoro.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Yanto Widiantoro, Sp.KK</div>
+			<h4>Spesialis Kulit dan Kelamin</h4>
+			<h3>Pukul : 11.00 - 14.00</h3>
+			<!-- <h3>Pukul : 10.00 - 12.00</h3>
+			<h3>Pukul : 10.00 - 12.00</h3> -->
+			<h5>- Status Pasien -</h5>
+		</div>
+
+		<div class="dok">
+			<a target="_blank" href="img\Sena Hadeaih.jpg">
+				<img src="img\Sena Hadeaih.jpg" width="300" height="300">
+			</a>
+			<div class="desc">dr. Sena Hadeaih, Sp.U</div>
+			<h4>Spesialis Urologi</h4>
+			<h3>Pukul : 14.00 - 16.00</h3>
+			<!-- <h3>Pukul : 10.00 - 12.00</h3>
+			<h3>Pukul : 10.00 - 12.00</h3> -->
+			<h5>- Status Pasien -</h5>
+			
+		</div>
+	</div>
+
+	<!-- <div class="running-txt">
+		<div class="container">
+			<h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit doloremque eos aspernatur modi et temporibus quod. Omnis laboriosam sint quasi quibusdam qui perferendis est sit nobis, a expedita ipsum magnam.</h2>
+		</div>
+	</div> -->
+
+	<div class="footer">
+        Copyright &copy; 2021
+        Designed by isfan.ff
+    </div
+	
+</body>
+
+</html>
